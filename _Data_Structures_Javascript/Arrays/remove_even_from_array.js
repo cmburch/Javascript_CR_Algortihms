beef844899ef08d66e89af46e0718793ce0ccd35
Charles Burch <georgia_beast89@yahoo.com>
2019-04-07 15:30:31 -0500

    Solved 2nd solution removed all the even numbers from array

--- _Data_Structures_Javascript/Arrays/remove_even_from_array.js
+++ _Data_Structures_Javascript/Arrays/remove_even_from_array.js
@@ -3,7 +3,7 @@
 
 
 //Solution 1
-//my solution
+//my solution i.e best solution
 
 let removeEven = (arr)=>{
     let readIndex = 0;
@@ -19,3 +19,29 @@ let removeEven = (arr)=>{
     
     return arr.splice(0,writeIndex);
 }
+// Runtime Complexity
+// O(n)
+
+// Memory Complexity
+// O(1)
+
+
+
+
+//Solution 2
+
+function removeEven(arr) {
+    var odds = []
+    for (let number of arr) {
+        if (number % 2 != 0) // Check if the item in the list is NOT even ('%' is the modulus symbol!)
+            odds.push(number) //If it isn't even append it to the empty list
+    }
+  return odds // Return the new list
+}
+console.log(removeEven([3, 2, 41, 3, 34]))
+
+// Runtime Complexity
+// O(n)
+
+// Memory Complexity
+// O(n)
