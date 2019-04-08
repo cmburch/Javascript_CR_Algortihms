26c9fa6be68d14452b64e949718f13841325f6b1
Charles Burch <georgia_beast89@yahoo.com>
2019-04-07 19:26:09 -0500

    solved findsum of value in array I sorted array and used moving indices

--- _Data_Structures_Javascript/Arrays/find_sum.js
+++ _Data_Structures_Javascript/Arrays/find_sum.js
@@ -4,8 +4,6 @@
 
 //Solution #1: Brute Force
 //this is the worst case
-
-
 function findSum(arr, value) {
     for (let j of arr) {
         for (let k of arr) {
@@ -23,3 +21,35 @@ console.log(findSum([1, 2, 3, 4], 10))
 
 // Memory Complexity
 // O(1)
+
+
+
+
+//Solution #2: Moving indices
+function findSum(arr, value) {
+
+    arr.sort(function(a, b) {
+        return a - b
+    })
+
+    var index1 = 0,
+        index2 = arr.length - 1,
+        result = [],
+        sum = 0;
+
+    while (index1 != index2) {
+        sum = arr[index1] + arr[index2]
+
+        if (sum < value) {
+            index1++;
+        } else if (sum > value) {
+            index2--;
+        } else {
+            result.push(arr[index1]);
+            result.push(arr[index2]);
+            return result;
+
+        }
+    }
+  return false;
+}
